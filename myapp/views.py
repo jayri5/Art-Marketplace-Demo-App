f691d1fdbbd111e82d8e76d0741cb6737518c28b
Jayri Bagchi <jayribagchi@gmail.com>
2023-06-09 18:00:23 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -7,9 +7,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 import razorpay
 from django.views.decorators.csrf import csrf_exempt
-from django.conf import settings
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
 # Create your views here.
 
 @login_required
